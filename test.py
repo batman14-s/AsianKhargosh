d29a4a8a7eb007c0fdaebe95f178056f3d27aa46
lazyXghost <rjtbnslrealme@gmail.com>
2021-12-24 21:33:30 +0530

    removed useless files

--- test.py
+++ test.py
@@ -1,13 +1,8 @@
-# pip install tensorflow
 from pyfirmata import Arduino, OUTPUT
 import tensorflow as tf
-import warnings
-warnings.filterwarnings('ignore')
 import numpy as np
 import cv2
-# from tf.keras.models import load_model
 
-facedetect = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap=cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
@@ -19,13 +14,6 @@ board = Arduino(port)
 
 pin = 3
 board.digital[pin].mode = OUTPUT
-
-def preprocessing(img):
-    img=img.astype("uint8")
-    img=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img=cv2.equalizeHist(img)
-    img = img/255
-    return img
 
 
 def get_className(classNo):
